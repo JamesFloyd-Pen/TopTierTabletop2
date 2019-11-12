5a146b57a53f7e5be43d75071506be7e0cfa733d
raspagator <ronaldvarela@live.com>
2019-11-12 14:03:33 -0500

    Added Firebase implementation

--- app/src/main/java/edu/floridapoly/mobiledeviceapp/fall19/toptiertabletop/DatabaseHelper.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapp/fall19/toptiertabletop/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package edu.floridapoly.mobiledeviceapp.fall19.toptiertabletop;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -15,29 +17,47 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     // Database Name
     private static final String DATABASE_NAME = "TopTierTabletop_db";
 
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_STORY = "story";
-    private static final String COLUMN_CHARACTER = "character";
-    private static final String COLUMN_PICTUREURL = "pictureurl";
+    private static final String KEY_ROWID = "_id";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
 
 
 
 
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         // create notes table
-        //db.execSQL(CREATE_TABLE);
+        db.execSQL("Create table user(email text primary key,password text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists user");
     }
+
+    public boolean insert(String email,String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email",email);
+        contentValues.put("password",password);
+        long ins = db.insert("user",null,contentValues);
+        if(ins==-1)return  false;
+        else return true;
+    }
+
+    //Check the email
+    public Boolean checkEmail(String email){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from user where email=?",new String[]{email});
+        if(cursor.getCount()>0) return false;
+        else return true;
+    }
+
+
 
 }
